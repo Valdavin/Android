d3d3c3b093fcbec8a7277c0381eaf8fdd91fdccf
Valentin Davin <dj_valentin@hotmail.fr>
2016-02-28 00:15:48 +0100

    Récup prénom

--- TP2/app/src/main/java/m4104c/tp2/HelloActivity.java
+++ TP2/app/src/main/java/m4104c/tp2/HelloActivity.java
@@ -2,12 +2,19 @@ package m4104c.tp2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class HelloActivity extends AppCompatActivity {
+public class
+        HelloActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        String prenom = getIntent().getStringExtra(Exercice4Activity.EXERCICE_4_PRENOM);
+
+        TextView Text_Prenom = (TextView) findViewById(R.id.activity_hello_text);
+        Text_Prenom.setText( "Hello " + prenom);
     }
 }
