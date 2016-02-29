836ff1a0f85713b68d250b167f5db67b67343ebd
Valentin Davin <dj_valentin@hotmail.fr>
2016-02-29 09:57:37 +0100

    repose, 4 - Etape 7

--- TP2/app/src/main/java/m4104c/tp2/Exercice4Activity.java
+++ TP2/app/src/main/java/m4104c/tp2/Exercice4Activity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class Exercice4Activity extends AppCompatActivity {
     public static final String EXERCICE_4_PRENOM = "prenom";
+    public static final int EXERCICE_4_HELLO_REQUEST = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +20,29 @@ public class Exercice4Activity extends AppCompatActivity {
     }
 
     public void Exercice4_valider(View view) {
-        TextView prenom_view = (TextView) findViewById(R.id.exercice4_prenom);
+        EditText prenom_view = (EditText) findViewById(R.id.exercice4_prenom);
         String prenom = prenom_view.getText().toString();
 
         Intent intent = new Intent(this,HelloActivity.class);
 
         intent.putExtra(EXERCICE_4_PRENOM, prenom);
 
-        startActivity(intent);
+        startActivityForResult(intent, EXERCICE_4_HELLO_REQUEST);
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == EXERCICE_4_HELLO_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "retour ok", Toast.LENGTH_SHORT).show();
+                EditText prenom_view = (EditText) findViewById(R.id.exercice4_prenom);
+                prenom_view.getText().clear();
 
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "retour cancel", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
 }
--- TP2/app/src/main/java/m4104c/tp2/HelloActivity.java
+++ TP2/app/src/main/java/m4104c/tp2/HelloActivity.java
@@ -1,7 +1,9 @@
 package m4104c.tp2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class
@@ -16,5 +18,15 @@ public class
 
         TextView Text_Prenom = (TextView) findViewById(R.id.activity_hello_text);
         Text_Prenom.setText( "Hello " + prenom);
+    }
+
+    public void changerPrenom(View view){
+        super.finish();
+    }
+
+    public void AutreExe(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
