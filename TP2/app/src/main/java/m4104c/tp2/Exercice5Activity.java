c67ea28c6c6ce4f4cfad3533dbf8d164f95f0247
Valentin Davin <dj_valentin@hotmail.fr>
2016-03-01 14:54:41 +0100

    presque maggle

--- TP2/app/src/main/java/m4104c/tp2/Exercice5Activity.java
+++ TP2/app/src/main/java/m4104c/tp2/Exercice5Activity.java
@@ -1,15 +1,34 @@
 package m4104c.tp2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.NumberPicker;
 
 
 public class Exercice5Activity extends AppCompatActivity {
-
+    public static final String EXERCICE_4_NUMERO = "numero";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercice5);
+        NumberPicker np = (NumberPicker)  findViewById(R.id.exercice5_numero);
+        np.setMinValue(1);
+        np.setMaxValue(20);
+    }
+
+    public void exercice5_valider(View view) {
+        NumberPicker np = (NumberPicker)  findViewById(R.id.exercice5_numero);
+        int numero = (int) np.getValue();
+
+        if (numero>0) {
+            Intent intent = new Intent(this,TableMultiplicationActivity.class);
+
+            intent.putExtra(EXERCICE_4_NUMERO,numero);
+
+            startActivity(intent);
+        }
     }
 
 }
--- TP2/app/src/main/java/m4104c/tp2/TableMultiplicationActivity.java
+++ TP2/app/src/main/java/m4104c/tp2/TableMultiplicationActivity.java
@@ -1,15 +1,75 @@
 package m4104c.tp2;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.TypeVariable;
 
 public class TableMultiplicationActivity extends AppCompatActivity {
-
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
+    public static int numeroTabl;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_multiplication);
 
-        addContentView();
+
+        numeroTabl = getIntent().getIntExtra(Exercice5Activity.EXERCICE_4_NUMERO, 42);
+
+        LinearLayout MainLayout = (LinearLayout) findViewById(R.id.table_multiplication_layout);
+        for (int i = 0; i<10; i++) {
+            LinearLayout ll = new LinearLayout(this);
+            ll.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            ll.setGravity(Gravity.CENTER);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView tw = new TextView(this);
+            tw.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            String textMult = numeroTabl + " x " + (i+1) + " = ";
+            tw.setText(textMult);
+            tw.setTextSize(25);
+
+            EditText et = new EditText(this);
+            et.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            et.setInputType(InputType.TYPE_CLASS_NUMBER);
+            et.setText("0");
+            et.setTextSize(15);
+
+            ll.addView(tw);
+            ll.addView(et);
+
+            MainLayout.addView(ll);
+
+
+
+
+        }
+    }
+
+    public void TableMultiplication_Valider(View view) {
+        int erreur = 0;
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.table_multiplication_layout);
+        for (int i = 0; i<10; i++){
+            LinearLayout ln = (LinearLayout) mainLayout.getChildAt(i+1);
+            EditText et = (EditText) ln.getChildAt(1);
+
+            int numero = Integer.parseInt(et.getText().toString());
+
+            if (numero == (i+1)*numeroTabl){
+                
+            }
+
+
+        }
     }
 }
