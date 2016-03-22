01312276d663080555053e896d5a0ac5e69129e0
Valentin <dj_valentin@hotmail.fr>
2016-03-23 00:20:31 +0100

    commit

--- Loustik/app/src/main/java/com/example/davinv/loustik/Login/FoumulaireUser.java
+++ Loustik/app/src/main/java/com/example/davinv/loustik/Login/FoumulaireUser.java
@@ -2,24 +2,14 @@ package com.example.davinv.loustik.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.davinv.loustik.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class FoumulaireUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void connexion(View view) {
-    }
-
-    public void newUser(View view) {
-    }
-
-    public void anonyme(View view) {
+        setContentView(R.layout.activity_foumulaire_user);
     }
 }
